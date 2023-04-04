c2506a03009806c0e14d4178708cc7d7bae450f8
lolorenzo777 <lolorenzo777@sunraylab.net>
2023-04-04 09:42:37 +0200

    ui.button beta1

--- pkg/ui/button.go
+++ pkg/ui/button.go
@@ -12,38 +12,59 @@ func init() {
 	ick.RegisterComposer("ick-button", &Button{})
 }
 
+// The button type can be one of the following:
+//
+//	BTN_TYPE_BUTTON  // <button> form buttons.
+//	BTN_TYPE_A       // <a> anchor links
+//	BTN_TYPE_SUBMIT  // <input type="submit"> submit inputs
+//	BTN_TYPE_RESET   // <input type="reset"> reset inputs
 type BUTTON_TYPE int
 
 const (
-	BTN_TYPE_BUTTON BUTTON_TYPE = iota // <button> form buttons. The default value
+	BTN_TYPE_BUTTON BUTTON_TYPE = iota // <button> form buttons.
 	BTN_TYPE_A                         // <a> anchor links
 	BTN_TYPE_SUBMIT                    // <input type="submit"> submit inputs
 	BTN_TYPE_RESET                     // <input type="reset"> reset inputs
 )
 
+// Button is an UISnippet registered with the ick-tag `ick-button`.
+//
+// According to the ButtonType property, Button can be used either as a standard <button> element but also as an anchor link or a submit or reset form input.
+// The core text is handle with the Title html property, allowing simple text or complex rendering.
+//
+// The IsDisabled property is directly handled by the embedded UISnippet.
 type Button struct {
 	dom.UISnippet
 
+	// The button type.
+	// If nothing is specified, the default ButtonType is BTN_TYPE_BUTTON.
 	ButtonType BUTTON_TYPE
-	HRef       url.URL
 
+	// If the ButtonType is BTN_TYPE_A then HRef defines the associated url link.
+	// HRef has no effect on other ButtonType. If HRef is defined then the button type is automatically set to BTN_TYPE_A.
+	HRef url.URL
+
+	// The title of the Button. Can be a simple text or a more complex html string.
 	Title html.String
 
-	IsOutlined bool
-	IsRounded  bool
-	IsLoading  bool
+	IsOutlined bool // Outlined button style
+	IsRounded  bool // Rounded button style
+
+	IsLoading bool // Loading button state
 
 	// TODO: handles buttons properties for color, size, display
 }
 
 func (_btn *Button) Template(*html.DataState) (_t html.SnippetTemplate) {
+	href := _btn.HRef.String()
+	if href != "" {
+		_btn.ButtonType = BTN_TYPE_A
+	}
 	switch _btn.ButtonType {
 	case BTN_TYPE_A:
 		_t.TagName = "a"
 		_t.Attributes = `class="button"`
-		if href := _btn.HRef.String(); href != "" {
-			_t.Attributes += ` href="` + href + `"`
-		}
+		_t.Attributes += ` href="` + href + `"`
 	case BTN_TYPE_SUBMIT:
 		_t.TagName = "input"
 		_t.Attributes = `class="button" type="submit"`
