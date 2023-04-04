c2f607b11db9aa8285b6f9a7aae13d6ab9fccf06
lolorenzo777 <lolorenzo777@sunraylab.net>
2023-04-04 09:40:28 +0200

    update examples

--- examples/example-element/wasm/example-element.go
+++ examples/example-element/wasm/example-element.go
@@ -20,24 +20,32 @@ func main() {
 
 	print("tagname", e.TagName())
 	print("id", e.Id())
+
 	e.SetClasses("myclass1 myclass2 myclass3 myclass6")
 	e.SetClasses("")
 	e.SwitchClasses("myclass3 myclass1", "myclass2 myclass4")
 	print("classes", e.Classes())
 	print("has class myclass1", sprintBool(e.HasClass("myclass1")))
 	print("has class myclass2", sprintBool(e.HasClass("myclass2")))
+
 	e.RemoveClasses("myclass2")
 	print("remove classes myclass2", e.Classes())
+
 	e.ResetClasses("myclass5")
 	print("reset myclass5", e.Classes())
+
 	e.ResetClasses("")
 	print("reset empty class", e.Classes())
+
 	e.SetStyle("color:red;")
 	print("style red", "color")
+
 	e.SetTabIndex(1)
 	print("tabindex", e.TabIndex())
+
 	e.SetDisabled(true)
 	print("Disabled", e.IsDisabled())
+
 	e.CreateAttribute("astring1", "string")
 	e.CreateAttribute("astring2", "string")
 	e.CreateAttribute("anum", 123)
@@ -47,9 +55,11 @@ func main() {
 	e.RemoveAttribute("astring1")
 	e.ToggleAttribute("abool2")
 	print("attributes", e.Attributes())
+
 	e.ToggleAttribute("abool2")
-	e.SetStyle("")
 	print("attributes", e.Attributes())
+
+	print("access key", "H")
 	e.SetAccessKey("H")
 
 	// let's go
