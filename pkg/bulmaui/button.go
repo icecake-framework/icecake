4d3a54f8d7d51fe294f5879e7d273665ff512921
lolorenzo777 <lolorenzo777@sunraylab.net>
2023-07-03 17:47:48 +0200

    refactoring button

--- pkg/bulmaui/button.go
+++ pkg/bulmaui/button.go
@@ -6,19 +6,13 @@ import (
 	"github.com/icecake-framework/icecake/pkg/html"
 )
 
-// Button is an UISnippet registered with the ick-tag `ick-button`.
-//
-// According to the ButtonType property, Button can be used either as a standard <button> element but also as an anchor link or a submit or reset form input.
-// The core text is handle with the Title html property, allowing simple text or complex rendering.
-//
-// The IsDisabled property is directly handled by the embedded UISnippet.
-type Button struct {
+type ICKButton struct {
 	*bulma.ICKButton
 	DOM dom.Element
 }
 
-func NewButton(title html.HTMLString, id string, rawURL string, attrs ...string) *Button {
-	btn := new(Button)
+func Button(title html.HTMLString, id string, rawURL string, attrs ...string) *ICKButton {
+	btn := new(ICKButton)
 	btn.ICKButton = bulma.Button(title, id, rawURL, attrs...)
 	return btn
 }
@@ -26,46 +20,46 @@ func NewButton(title html.HTMLString, id string, rawURL string, attrs ...string)
 // returns nil if the id does not exists or if it's not a Navbar
 //
 // TODO: implement a generic wrapper to any event handlers
-func WrapButton(id string) *Button {
+func WrapButton(id string) *ICKButton {
 	e := dom.RenderedId(id, "ick-button")
 	if e == nil {
 		return nil
 	}
-	n := new(Button)
+	n := new(ICKButton)
 	n.DOM.Wrap(e)
 	return n
 }
 
-func (btn *Button) SetOutlined(f bool) *Button {
+func (btn *ICKButton) SetOutlined(f bool) *ICKButton {
 	btn.ICKButton.SetOutlined(f)
 	btn.DOM.SetClassIf(f, "is-outlined")
 	return btn
 }
 
-func (btn *Button) SetRounded(f bool) *Button {
+func (btn *ICKButton) SetRounded(f bool) *ICKButton {
 	btn.ICKButton.SetRounded(f)
 	btn.DOM.SetClassIf(f, "is-rounded")
 	return btn
 }
 
-func (btn *Button) SetDisabled(disabled bool) {
+func (btn *ICKButton) SetDisabled(disabled bool) {
 	btn.ICKButton.SetDisabled(disabled)
 	btn.DOM.SetDisabled(disabled)
 }
 
-func (btn *Button) SetLoading(f bool) *Button {
+func (btn *ICKButton) SetLoading(f bool) *ICKButton {
 	btn.ICKButton.SetLoading(f)
 	btn.DOM.SetClassIf(f, "is-loading")
 	return btn
 }
 
-func (btn *Button) SetColor(c bulma.COLOR) *Button {
+func (btn *ICKButton) SetColor(c bulma.COLOR) *ICKButton {
 	btn.ICKButton.SetColor(c)
 	btn.DOM.PickClass(bulma.COLOR_OPTIONS, string(c))
 	return btn
 }
 
-func (btn *Button) SetLight(f bool) *Button {
+func (btn *ICKButton) SetLight(f bool) *ICKButton {
 	btn.ICKButton.SetLight(f)
 	btn.DOM.SetClassIf(f, "is-light")
 	return btn
