263cb9180b18d6451ce203c3f7468c021bbcaeef
lolorenzo777 <lolorenzo777@sunraylab.net>
2023-07-05 19:54:15 +0200

    refactoring

--- pkg/bulma/delete.go
+++ pkg/bulma/delete.go
@@ -4,38 +4,40 @@ import (
 	"io"
 
 	"github.com/icecake-framework/icecake/pkg/html"
+	"github.com/lolorenzo777/verbose"
 )
 
 func init() {
-	html.RegisterComposer("ick-delete", &Delete{})
+	html.RegisterComposer("ick-delete", &ICKDelete{})
 }
 
-type Delete struct {
+type ICKDelete struct {
 	html.HTMLSnippet
 
 	// the element id to remove from the DOM when the delete button is clicked
-	TargetID string
+	TargetId string
 
 	// styling
 	SIZE
 }
 
 // Ensure Delete implements HTMLTagComposer interface
-var _ html.HTMLTagComposer = (*Delete)(nil)
+var _ html.HTMLComposer = (*ICKDelete)(nil)
 
 // BuildTag builds the tag used to render the html element.
 // Delete tag is a simple <button class="delete"></delete>
-func (del *Delete) BuildTag() html.Tag {
+func (del *ICKDelete) BuildTag() html.Tag {
 	del.Tag().
 		SetTagName("button").
 		AddClass("delete").
 		SetAttribute("aria-label", "delete").
-		SetAttribute("data-TargetId", del.TargetID).
+		SetAttributeIf(del.TargetId != "", "data-targetid", del.TargetId).
 		PickClass(SIZE_OPTIONS, string(del.SIZE))
+	verbose.PrintfIf(del.TargetId == "", verbose.WARNING, "ICKDelete.BuildTag: missing TargetId\n")
 	return *del.Tag()
 }
 
 // Delete renders an empty content.
-func (del *Delete) RenderContent(out io.Writer) error {
+func (del *ICKDelete) RenderContent(out io.Writer) error {
 	return nil
 }
