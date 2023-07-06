99558e3a42fe123d829b63b3fafea68f3832a7ce
lolorenzo777 <lolorenzo777@sunraylab.net>
2023-07-06 08:25:53 +0200

    refactoring doc message

--- web/docs/pages/bulmamessage.go
+++ web/docs/pages/bulmamessage.go
@@ -14,22 +14,25 @@ const (
 type SectionBulmaMessage struct{ SectionIcecakeDoc }
 
 func (sec *SectionBulmaMessage) RenderContent(out io.Writer) error {
-	html.WriteString(out, `<h2>Bulma Message</h2>`)
-	html.WriteString(out, `<p>bulma.Message is an icecake snippet providing the HTML rendering for a `, linkBulmaMessage, ` with extra features and usefull Go APIs.</p>`)
+	html.WriteString(out, `<h2>Bulma Message</h2>`+
+		`<p>bulma.Message is an icecake snippet providing the HTML rendering for a `, linkBulmaMessage, ` with extra features and usefull Go APIs.</p>`)
 
 	// apis
-	html.WriteString(out, `<h3>bulma.ICKMessage API</h3>`)
-	html.WriteString(out, `<p><code>Message(cmp html.HTMLComposer) *ICKMessage</code> is the Message factory.</p>`)
-	html.WriteString(out, `<p><code>.SetHeader(header html.HTMLString, candelete bool)</code> set a header with or without the delete button.</p>`)
-	html.WriteString(out, `<p>The Message is an HTMLSnippet so you can use <code>AddContent</code> to setup the content of the message.</p>`)
-	html.WriteString(out, `<p><code>.AddContent(cmp html.HTMLComposer) error</code> Stack content inside.</p>`)
+	html.WriteString(out, `<h3>bulma.ICKMessage API</h3>`+
+		`<p><code>Message(content html.HTMLComposer) *ICKMessage</code> is the main Message factory.</p>`+
+		`<p><code>.SetHeader(header html.HTMLString, candelete bool)</code> set a header with or without the delete button.</p>`+
+		`<p>The Message is an HTMLSnippet so you can use <code>AddContent</code> to setup the content of the message.</p>`+
+		`<p><code>.AddContent(cmp html.HTMLComposer) error</code> Stack content inside.</p>`)
 
 	// rendering
 	html.WriteString(out, `<h3>Usage</h3>`)
-	sec.RenderChild(out, bulma.Button(*html.ToHTML("reset"), "btnreset", "", `class="mb-3"`).SetColor(bulma.COLOR_PRIMARY).SetOutlined(true).SetDisabled(true))
-	html.WriteString(out, `<div id="boxusage" class="box mr-5">`)
-	html.Render(out, nil, bulma.Spinner())
-	html.WriteString(out, `</div>`)
+	html.Render(out, nil, bulma.Button(*html.ToHTML("reset"), "btnreset", "", `class="mb-3"`).
+		SetColor(bulma.COLOR_PRIMARY).
+		SetOutlined(true).
+		SetDisabled(true))
+	html.Render(out, nil, html.Div(`class="box mr-5"`).
+		SetId("boxusage").
+		AddContent(bulma.Spinner()))
 
 	// rendering
 	html.WriteString(out, `<h3>Rendering</h3>`)
@@ -41,21 +44,20 @@ func (sec *SectionBulmaMessage) RenderContent(out io.Writer) error {
 	// DEBUG: dump the map of sub composers
 
 	r4 := bulma.Message(nil).SetHeader(*html.ToHTML("Only header")).SetDeletable("msgr4")
-	html.Render(out, sec, r1, r2, r3, r4)
+	html.Render(out, nil, r1, r2, r3, r4)
 	html.WriteString(out, `</div>`)
 
 	// styling
 	html.WriteString(out, `<h3>Styling</h3>`)
 	html.WriteString(out, `<div class="box mr-5">`)
+
 	styl1 := bulma.Message(html.ToHTML("Make use of bulma.COLOR property.<br>COLOR = COLOR_PRIMARY")).SetHeader(*html.ToHTML("Icecake Message")).
 		SetColor(bulma.COLOR_INFO)
 	styl2 := bulma.Message(html.ToHTML("Make use of bulma.SIZE property.<br>SIZE = SIZE_SMALL")).SetHeader(*html.ToHTML("Icecake Message")).
 		SetSize(bulma.SIZE_SMALL)
+	html.Render(out, nil, styl1, styl2)
 
-	html.Render(out, sec, styl1, styl2)
 	html.WriteString(out, `</div>`)
-
-	// states
 
 	return nil
 }
