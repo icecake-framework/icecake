448afbae843cd0fdf7477c6169b563c7ca434c09
lolorenzo777 <lolorenzo777@sunraylab.net>
2023-07-06 08:49:48 +0200

    doc card

--- web/docs/pages/bulmacard.go
+++ web/docs/pages/bulmacard.go
@@ -14,35 +14,30 @@ const (
 type SectionBulmaCard struct{ SectionIcecakeDoc }
 
 func (cmp *SectionBulmaCard) RenderContent(out io.Writer) error {
-	html.WriteString(out, `<h2>Bulma Card</h2>`)
-	html.WriteString(out, `<p>bulma.Card is an icecake snippet providing the HTML rendering for a `, linkBulmaCard, ` with extra features and usefull Go APIs.</p>`)
+	html.WriteString(out, `<h2>Bulma Card</h2>`+
+		`<p>bulma.Card is an icecake snippet providing the HTML rendering for a `+linkBulmaCard+` with extra features and usefull Go APIs.</p>`)
 
 	// api
-	html.WriteString(out, `<h3>bulma.Card API</h3>`)
-	html.WriteString(out, `<p>The Card is an HTMLSnippet. Use <code>SetContent()</code> to setup the content of the card.</p>`)
-	html.WriteString(out, `<p><strong>Title HTMLString</strong> Optional title to display in the head of the card. Can be a simple text or a more complex html string.</p>`)
-	html.WriteString(out, `<p><strong>Image *Image</strong> Optional image to display on top of the card.</p>`)
+	html.WriteString(out, `<h3>bulma.Card API</h3>`+
+		`<p>The Card is an HTMLSnippet. Use <code>AddContent()</code> to setup the content of the card.</p>`+
+		`<p><strong>Title HTMLString</strong> Optional title to display in the head of the card. Can be a simple text or a more complex html string.</p>`+
+		`<p><strong>Image *Image</strong> Optional image to display on top of the card.</p>`)
 
 	// usages
-	html.WriteString(out, `<h3>Usage</h3>`)
-	html.WriteString(out, `<div class="box is-flex spaceout mr-5">`)
+	html.WriteString(out, `<h3>Usage</h3>`+
+		`<div class="box is-flex spaceout mr-5">`)
 
-	card3 := bulma.NewCard()
-	card3.AddContent(html.ToHTML(`<p class="title">Very Good Cake</p>`))
-	card3.Tag().AddStyle("width: 150px;")
-	cmp.RenderChild(out, card3)
+	u1 := bulma.Card(html.ToHTML(`<p class="title">Very Good Cake</p>`))
+	u1.Tag().AddStyle("width: 150px;")
+	html.Render(out, nil, u1)
 
-	card1 := bulma.NewCard()
-	card1.SetTitle(*html.ToHTML("Hello World")).
+	html.Render(out, nil, bulma.Card(html.ToHTML("Nice cake")).
+		SetTitle(*html.ToHTML("Hello World")).
 		SetImage(*bulma.NewImage("/assets/icecake.jpg", bulma.IMG_128x128)).
-		AddFooterItem(*html.ToHTML("<a href='/'>home</a>")).
-		AddContent(html.ToHTML("Nice cake"))
-	cmp.RenderChild(out, card1)
+		AddFooterItem(*html.ToHTML("<a href='/'>home</a>")))
 
-	card2 := bulma.NewCard()
-	card2.SetImage(*bulma.NewImage("/assets/icecake.jpg", bulma.IMG_64x64)).
-		AddContent(html.ToHTML("Nice cake"))
-	cmp.RenderChild(out, card2)
+	html.Render(out, nil, bulma.Card(html.ToHTML("Nice cake")).
+		SetImage(*bulma.NewImage("/assets/icecake.jpg", bulma.IMG_64x64)))
 
 	html.WriteString(out, `</div>`)
 
