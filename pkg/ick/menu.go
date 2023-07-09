2f273a8a545c421e4a370cba949e49328d1023b8
lolorenzo777 <lolorenzo777@sunraylab.net>
2023-07-09 10:23:32 +0200

    doc: menu

--- pkg/ick/menu.go
+++ pkg/ick/menu.go
@@ -98,25 +98,42 @@ func (mnui *IckMenuItem) RenderContent(out io.Writer) error {
 	return nil
 }
 
-// IckMenu is an icecake snippet providing the HTML rendering for a [bulma menu].
+type MENU_TYPE string
+
+const (
+	MENUTYP_MENU  MENU_TYPE = "menu"
+	MENUTYP_NAV   MENU_TYPE = "nav"
+	MENUTYP_ASIDE MENU_TYPE = "aside"
+)
+
+// ICKMenu is an icecake snippet providing the HTML rendering for a [bulma menu].
 //
 // [bulma menu]: https://bulma.io/documentation/components/menu
-type IckMenu struct {
+type ICKMenu struct {
 	html.BareSnippet
 
-	menuTag html.Tag // menu tag: nav, aside, menu. <menu> is used if nothing is specified. Cna be used to setup some classes like "is-small"
+	MENU_TYPE
+	SIZE
 
 	items []*IckMenuItem // list of Menu items
 }
 
 // Ensuring ICKMenu implements the right interface
-var _ html.ElementComposer = (*IckMenu)(nil)
+var _ html.ElementComposer = (*ICKMenu)(nil)
+
+func Menu(id string, attrs ...string) *ICKMenu {
+	n := new(ICKMenu)
+	n.Tag().ParseAttributes(attrs...)
+	n.Tag().SetId(id)
+	return n
+}
 
 // Clone clones this Menu and all its items and subitem, keeping their attributes their item index and their key.
-func (src IckMenu) Clone() *IckMenu {
-	clone := new(IckMenu)
+func (src ICKMenu) Clone() *ICKMenu {
+	clone := new(ICKMenu)
 	clone.BareSnippet = *src.BareSnippet.Clone()
-	clone.menuTag = *src.menuTag.Clone()
+	clone.MENU_TYPE = src.MENU_TYPE
+	clone.SIZE = src.SIZE
 	clone.items = make([]*IckMenuItem, len(src.items))
 	for i, itm := range src.items {
 		clone.items[i] = itm.Clone()
@@ -124,16 +141,19 @@ func (src IckMenu) Clone() *IckMenu {
 	return clone
 }
 
-func (mnu *IckMenu) MenuTag() *html.Tag {
-	if mnu.menuTag.AttributeMap == nil {
-		mnu.menuTag.AttributeMap = make(html.AttributeMap)
-	}
-	return &mnu.menuTag
+func (mnu *ICKMenu) SetType(t MENU_TYPE) *ICKMenu {
+	mnu.MENU_TYPE = t
+	return mnu
+}
+
+func (mnu *ICKMenu) SetSize(sz SIZE) *ICKMenu {
+	mnu.SIZE = sz
+	return mnu
 }
 
 // SetActiveItem look for the key item (or subitem) and sets its IsActive flag.
 // warning: does not unset other actve items if any.
-func (mnu *IckMenu) SetActiveItem(key string) *IckMenu {
+func (mnu *ICKMenu) SetActiveItem(key string) *ICKMenu {
 	itm := mnu.Item(key)
 	if itm != nil {
 		itm.IsActive = true
@@ -142,7 +162,7 @@ func (mnu *IckMenu) SetActiveItem(key string) *IckMenu {
 }
 
 // AddItem adds the item to the Menu
-func (mnu *IckMenu) AddItem(key string, itmtyp MENUITEM_TYPE, txt string) *IckMenuItem {
+func (mnu *ICKMenu) AddItem(key string, itmtyp MENUITEM_TYPE, txt string) *IckMenuItem {
 	itm := new(IckMenuItem)
 	itm.Key = key
 	itm.Type = itmtyp
@@ -153,7 +173,7 @@ func (mnu *IckMenu) AddItem(key string, itmtyp MENUITEM_TYPE, txt string) *IckMe
 
 // At returns the item at a given index.
 // returns nil if index is out of range.
-func (mnu *IckMenu) At(index int) *IckMenuItem {
+func (mnu *ICKMenu) At(index int) *IckMenuItem {
 	if index < 0 || index >= len(mnu.items) {
 		return nil
 	}
@@ -162,7 +182,7 @@ func (mnu *IckMenu) At(index int) *IckMenuItem {
 
 // Item returns the first item found with the given key, walking through all levels.
 // returns nil if key is not found
-func (mnu *IckMenu) Item(key string) *IckMenuItem {
+func (mnu *ICKMenu) Item(key string) *IckMenuItem {
 	for _, itm := range mnu.items {
 		if itm.Key == key {
 			return itm
@@ -172,7 +192,7 @@ func (mnu *IckMenu) Item(key string) *IckMenuItem {
 }
 
 // BuildTag builds the tag used to render the html element.
-func (mnu *IckMenu) BuildTag() html.Tag {
+func (mnu *ICKMenu) BuildTag() html.Tag {
 	mnu.Tag().SetTagName("div")
 
 	// set style height if there's a footer
@@ -187,13 +207,13 @@ func (mnu *IckMenu) BuildTag() html.Tag {
 }
 
 // RenderContent writes the HTML string corresponding to the content of the HTML element.
-func (mnu *IckMenu) RenderContent(out io.Writer) error {
-	mnutag := mnu.menuTag.Clone()
-	if tagname, _ := mnutag.TagName(); tagname == "" {
-		mnutag.SetTagName("menu")
+func (mnu *ICKMenu) RenderContent(out io.Writer) error {
+	typ := mnu.MENU_TYPE
+	if typ == "" {
+		typ = MENUTYP_MENU
 	}
-	mnutag.AddClass("menu")
-	mnutag.SetAttribute("role", "navigation")
+	mnutag := html.NewTag(string(typ), `class="menu" role="navigation"`)
+	mnutag.AddClass(string(mnu.SIZE))
 	mnutag.RenderOpening(out)
 
 	lastlevel := 0
@@ -245,7 +265,8 @@ func (mnu *IckMenu) RenderContent(out io.Writer) error {
 
 	// add footer
 	hasfooter := false
-	foottag := html.NewTag("div", html.ParseAttributes(`class="`+mnutag.Classes()+`"`))
+	foottag := html.NewTag("div")
+	foottag.AddClass(string(mnu.SIZE))
 	for _, item := range mnu.items {
 		if item.Type == MENUIT_FOOTER {
 			if !hasfooter {
--- web/docs/pages/docmenu.go
+++ web/docs/pages/docmenu.go
@@ -2,12 +2,27 @@ package webdocs
 
 import (
 	"io"
+
+	"github.com/icecake-framework/icecake/pkg/html"
+	"github.com/icecake-framework/icecake/pkg/ick"
 )
 
 type SectionDocMenu struct{ SectionDocIcecake }
 
 func (sec *SectionDocMenu) RenderContent(out io.Writer) error {
-	sec.RenderHead(out, "Menue", "menu.go", "ICKMenu")
+	sec.RenderHead(out, "Menu", "menu.go", "ICKMenu")
+
+	// usages
+	html.RenderString(out, `<div class="box spaceout">`)
+	u1 := ick.Menu("u1")
+	u1.AddItem("", ick.MENUIT_LABEL, "Label")
+	u1.AddItem("", ick.MENUIT_LINK, "link 1")
+	u1.AddItem("", ick.MENUIT_LINK, "link 2")
+	u1.AddItem("", ick.MENUIT_NESTEDLINK, "Nested Link 1")
+	u1.AddItem("", ick.MENUIT_NESTEDLINK, "Nested Link 2")
+	u1.AddItem("", ick.MENUIT_LINK, "link 3")
+	html.RenderChild(out, sec, u1)
+	html.RenderString(out, `</div>`)
 
 	return nil
 }
