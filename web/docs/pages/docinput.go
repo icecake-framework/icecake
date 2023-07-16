2d127c5d0a9147aefd0cc0c99a0704f5bbc660fc
lolorenzo777 <lolorenzo777@sunraylab.net>
2023-07-16 05:56:23 +0200

    doc: inputfield

--- web/docs/pages/docinput.go
+++ web/docs/pages/docinput.go
@@ -10,7 +10,7 @@ import (
 type SectionDocInput struct{ SectionDocIcecake }
 
 func (sec *SectionDocInput) RenderContent(out io.Writer) error {
-	sec.RenderHead(out, "InputField", "input.go", "ICKInput")
+	sec.RenderHead(out, "ICKInputField", "inputfield.go", "ICKInputField")
 
 	// usages
 	ickcore.RenderChild(out, sec,
